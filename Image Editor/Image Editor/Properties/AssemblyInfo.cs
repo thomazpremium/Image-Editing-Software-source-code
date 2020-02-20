5776fae52868afe7896ff68e7254920166c2a6ec
thomazpremium <61012522+thomazpremium@users.noreply.github.com>
2020-02-20 15:32:17 -0300

    AssemblyInfo.cs

--- Image Editor/Image Editor/Properties/AssemblyInfo.cs
+++ Image Editor/Image Editor/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Image Editor")]
+[assembly: AssemblyTitle("design")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Image Editor")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCompany("tota")]
+[assembly: AssemblyProduct("design")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
